813c4da0f73ef7182a3d508d59003c94646b99d2
ShreyaSawant2000 <92082171+ShreyaSawant2000@users.noreply.github.com>
2024-08-26 09:52:02 +0530

    Update db.js

--- auth-user/config/db.js
+++ auth-user/config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password:'shreya1234',
+    password:'your-password',
     database:'authrole'
 });
 
